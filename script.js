8a538e50d832f1ff43a65c08f60c902d509f387e
kent28808 <kktanoue@gmail.com>
2020-09-23 23:27:10 -0700

    Added High Scores HTML.  Need to link

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Variables for Code Quiz
 var minutesDisplay = document.querySelector("#minutes");
 var secondsDisplay = document.querySelector("#seconds");
 var startButton = document.querySelector("#start-btn");
@@ -7,9 +8,12 @@ var ButtonC = document.querySelector("#C");
 var ButtonD = document.querySelector("#D");
 var Question = document.querySelector("#Q");
 
-
 var totalSeconds = 300;
 var interval;
+var score = 0;
+var questionIndex = 0;
+var penalty = 10;
+
 // Array and Object for code questions.
 var questions = [{
     Q: "Which dinosaur is classified as a carnivore?",
@@ -17,13 +21,15 @@ var questions = [{
     B: "Triceratops",
     C: "T-Rex",
     D: "Your pet cat",
-}, 
+    answer: "C:T-Rex"
+},
 {
     Q: "When did dinosaurs go extinct?",
     A: "Yesterday",
     B: "Last week",
     C: "65 million years ago",
     D: "1000 years ago",
+    answer: "C:65 million years ago"
 },
 {
     Q: "On which continent have the most dinosaur fossils been found?",
@@ -31,6 +37,7 @@ var questions = [{
     B: "Africa",
     C: "Asia",
     D: "Hawaii",
+    answer: "A:North America"
 },
 {
     Q: "How did Stegosaurus protect itself from enemies?",
@@ -38,17 +45,19 @@ var questions = [{
     B: "Stomped on them",
     C: "Smacked them with its spiky tail",
     D: "Bit them",
+    answer: "C:Smacked them with its spiky tail"
 },
 {
     Q: "If a T-Rex were chasing you across a field, how could you outrun this predator?",
     A: "Call an uber",
     B: "By walking",
     C: "By riding a skateboard",
-    D: "By zooming away on a motorized scooter",
+    D: "By zooming away on a motorized bike",
+    answer: "D:By zooming away on a motorized bike"
 }
+];
 
-]; 
-
+//Functions for Countdown Timer
 function setTime() {
     clearInterval(interval)
 
@@ -82,7 +91,6 @@ function getFormattedSeconds() {
     return formattedSeconds;
 }
 
-
 function renderTime() {
     minutesDisplay.textContent = getFormattedMinutes();
     secondsDisplay.textContent = getFormattedSeconds();
@@ -109,14 +117,14 @@ function stopTimer() {
 
 function startgame() {
     startTimer();
-    for(var i = 0; i < questions.length; i++){
+    for (var i = 0; i < questions.length; i++) {
         var question = questions[i];
         renderquestion(question);
-        if(response == questions[i].answer){
+        if (response == questions[i].answer) {
             score++;
         }
     }
-    
+
 }
 
 function renderquestion(question) {
@@ -125,14 +133,23 @@ function renderquestion(question) {
     ButtonC.textContent = question.C;
     ButtonD.textContent = question.D;
     Question.textContent = question.Q;
-    
+
 }
 
 //function selectAnswer() {}
 
 //function reset answers
 
+//function subtract 10s for incorrect answers
+
+//function save highscore
+
 startButton.addEventListener("click", startgame);
+
+
+
+
+
 
 
 
